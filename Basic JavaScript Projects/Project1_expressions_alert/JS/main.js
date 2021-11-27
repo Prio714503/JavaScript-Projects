e08eb25c44c33436e4c140e22855d94191314c8e
Prio714503 <prios_360@msn.com>
2021-11-27 11:23:29 -0800

    Submission Assignment
    Javascript practice.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,42 @@
+
+//Window.alert method
+window.alert()
+document.write("Hello, World!");
+
+//Document write method
+var x = "Hello, World!";
+document.write(x);
+
+//Assign a variable to a string
+var x = "My first string";
+document.write(x);
+
+//Assign a string to a variable
+var x = "";
+window.alert("Learning Java is cool!");
+
+//Escape character
+document.write("then I said, \"Javascript can be learned!\" says the student.");
+
+//Concatenating a string
+document.write("\"If you don\'t stand for something,"+"You\'ll fall for anything!");
+
+//Concatenated string
+var x = "Apples" + "Bananas";
+document.write(x);
+
+//Assign muliple variables in one statement and using the fontcolor method
+var cars = "Sports cars"; Audi = "RS-7", Mercedes = "AMG 63s", BMW = "M8";
+var cars = cars.fontcolor("red");
+document.write(Mercedes);
+
+//Expressions
+let obj = {};
+obj.x = 3;
+document.write(obj.x);
+
+
+
+
+
+
