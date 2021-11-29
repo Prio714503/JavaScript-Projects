919490afc94bd7bdabf04465dec6f8433b4a5659
Prio714503 <prios_360@msn.com>
2021-11-28 17:33:15 -0800

    Project2 functions assignment
    Assignment submission

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,6 +1,23 @@
+//Using the += operator to concatinate a string in a function
 
 function myFunction() {
     var sentence = "It is raining";
     sentence += "you need an umbrella!";
     document.getElementById("Concatenate").innerHTML = sentence;
 }
+
+// Function practice
+
+function ShowMessage(firstName, lastName) {
+    alert("Hello " + firstName + " " + lastName);
+}
+ShowMessage("Phillip", "Rios");
+
+
+// Function expression
+
+ var add = function sum(val1, val2) {
+    return val1 + val2;
+};
+document.getElementById("p1").innerHTML = add(20,20);
+document.getElementById("p2").innerHTML = sum(20,20);
