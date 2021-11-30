85ddd0b9d03a27e44a8d99b002d0c8b5d41e1521
Prio714503 <prios_360@msn.com>
2021-11-30 14:59:35 -0800

    Dictionaries Assignment

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,39 @@
+
+//Dictionary example
+function my_Dictionary () {
+    var Animal = {
+        species:"Dog",
+        Color:"Black",
+        Breed:"Pitbull",
+        Age:5,
+        Sound:"Bark!" 
+    };
+    document.getElementById("Dictionary").innerHTML = Animal.Breed;
+}
+
+//Created a dictionary containing Key Value Pairs
+function my_carCollection () {
+    var Sportscar = {
+        Bodystyle:"Coupe",
+        Color:"red",
+        Engine:"Gas",
+        Trim:"RS",
+        Year:2021
+    };
+    document.getElementById("AudiRS").innerHTML = Sportscar.Bodystyle;
+    
+}
+
+
+//Operators as words
+function my_eCarCollection () {
+    var electricCar = {
+        Bodystyle:"Coupe",
+        Color:"red",
+        Drivetrain:"Eletric",
+        Trim:"ES",
+        Year:2022
+    };
+    delete electricCar.Trim;
+    document.getElementById("PoleStar").innerHTML = electricCar.Trim;
+}
