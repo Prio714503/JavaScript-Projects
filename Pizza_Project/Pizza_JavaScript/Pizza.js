8c3d7a2257168ae9d543401e6233d40c9effe190
Prio714503 <prios_360@msn.com>
2021-12-14 18:45:40 -0800

    Pizza Project Assignment

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -1,9 +1,7 @@
-
-
 function getReceipt() {
     //This initializes our string so it can get passed from
-    // function to function, growing line by line into a full receipt.
-    var text1 = "<h3>You Ordered</h3>";
+    //function to function, growing line by line into a full receipt.
+    var text1 = "<h3>You Ordered:</h3>";
     var runningTotal = 0;
     var sizeTotal = 0;
     var sizeArray = document.getElementsByClassName("size");
@@ -14,7 +12,7 @@ function getReceipt() {
         }
     }
     if (selectedSize === "Personal Pizza") {
-        sizeTotal = 6
+        sizeTotal = 6;
     } else if (selectedSize === "Small Pizza") {
         sizeTotal = 8;
     } else if (selectedSize === "Medium Pizza") {
@@ -24,13 +22,16 @@ function getReceipt() {
     } else if (selectedSize === "Extra Large Pizza") {
         sizeTotal = 16;
     }
+
     runningTotal = sizeTotal;
     console.log(selectedSize+" = $"+sizeTotal+".00");
     console.log("size text1: "+text1);
     console.log("subtotal: $"+runningTotal+".00");
-    // these variables will get passed on to each function
+    //These variables will get passed on to each function.
     getTopping(runningTotal,text1);
+    
 };
+
 
 function getTopping(runningTotal,text1) {
     var toppingTotal = 0;
@@ -43,20 +44,20 @@ function getTopping(runningTotal,text1) {
             text1 = text1+toppingArray[j].value+"<br>";
         }
     }
+
     var toppingCount = selectedTopping.length;
     if (toppingCount > 1) {
         toppingTotal = (toppingCount - 1);
     } else {
         toppingTotal = 0;
     }
-    runningTotal = (runningTotal + toppingTotal);
+
+    runningTotal = (runningTotal + runningTotal);
     console.log("total selected topping items: "+toppingCount);
     console.log(toppingCount+" topping - 1 free topping = "+"$"+toppingTotal+".00");
     console.log("topping text1: "+text1);
-    console.log("Purchase Total: "+"$"+runningTotal+".00");
+    console.log("Purchase total: "+"$"+runningTotal+".00");
     document.getElementById("showText").innerHTML=text1;
     document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+
     runningTotal+".00"+"</strong></h3>";
 };
-
-
